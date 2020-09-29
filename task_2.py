4e1411d6dfa5aae30db3c0e4c0a209c8bb9cc477
eyakovleva <ekaterinajakovl8@gmail.com>
2020-09-30 00:17:06 +0300

    сделала 2-е задание из 1-го урока "Основы языка Python" по Python

---
+++ task_2.py
@@ -0,0 +1,6 @@
+script_time = int(input("How long did it take for your PC to run a script? "))
+seconds = script_time % 60
+minutes_overall = script_time // 60
+minutes = minutes_overall % 60
+hours = minutes_overall // 60
+print(f'{hours:02}:{minutes:01}:{seconds:01}')
